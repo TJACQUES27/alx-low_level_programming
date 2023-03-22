3d97b2c0e3ddc1be616285b4d626c8f91a3ba19f
TJACQUES27 <twahirwajacques27@gmail.com>
2023-03-22 12:22:57 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -16,16 +16,4 @@ void print_alphabet(void)
 		l += 1;
 	}
 	_putchar(10);
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int:wq
-	    main(void)
-{
-    print_alphabet();
-    return (0);
-}}
+}
